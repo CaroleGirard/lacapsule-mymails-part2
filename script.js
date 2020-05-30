d84b36b62a5820bf9a68ffbe6fc7a5223f770f28
Carole Girard <kca.girard@gmail.com>
2020-05-30 14:44:29 +0200

    Supprimer des messages

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 document.getElementById('compteur').textContent = document.getElementsByTagName('p').length;
+
+for (var i=0; i<document.getElementsByClassName('trash').length; i++) {
+    document.getElementsByClassName('trash')[i].addEventListener("click",
+    function (){
+        this.parentNode.remove()
+    }
+    )
+};
