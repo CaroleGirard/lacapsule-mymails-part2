81e3dbddcc0ddb86db35d8b94a108244245bdf8b
Carole Girard <kca.girard@gmail.com>
2020-06-01 18:44:19 +0200

    Mise à jour du compteur après ajout d'un nouveau message

--- script.js
+++ script.js
@@ -10,9 +10,27 @@ for (var i=0; i<document.getElementsByClassName('trash').length; i++) {
 
 
 document.getElementById('bouton').addEventListener("click",
-function () {
-    var newDiv= document.body.appendChild(document.createElement("div"))
-    var newMessage = document.createTextNode(document.getElementById('champs').value)
-    newDiv.appendChild(newMessage)
+function () { 
+    var newDiv= document.createElement("div");
+    document.body.appendChild(newDiv);
+    newDiv.className='row';
+    var newAvatar= document.createElement("img");
+    newDiv.appendChild(newAvatar);
+    newAvatar.src = 'avatar-5.jpg';
+    newAvatar.className= 'avatar';
+    var prenom= document.createElement("h6");
+    newDiv.appendChild(prenom);
+    prenom.textContent = 'Carole Girard';
+    var p= document.createElement("p");
+    newDiv.appendChild(p);
+    var newMessage = document.createTextNode(document.getElementById('champs').value);
+    p.appendChild(newMessage);
+    var newTrash= document.createElement("img");
+    newDiv.appendChild(newTrash);
+    newTrash.src = "trash.png";
+    newTrash.className= "trash";
+    document.getElementById('champs').value = " "
+    document.getElementById('compteur').textContent = document.getElementsByTagName('p').length;
 }
 );
+
